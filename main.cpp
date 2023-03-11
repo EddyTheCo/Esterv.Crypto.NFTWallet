dbf94748e4ae806ae576453be57d012f1886ed68
EddyGlezz <eddyglezz95@gmail.com>
2023-03-11 23:06:00 +0100

    change the company name

--- main.cpp
+++ main.cpp
@@ -10,13 +10,13 @@ int main(int argc, char *argv[])
 
 	QQmlApplicationEngine engine;
 	engine.addImageProvider(QLatin1String("qrcodeblack"), new QRImageProvider("black",1));
-    engine.addImportPath("qrc:/esthertech.com/imports");
+    engine.addImportPath("qrc:/esterVtech.com/imports");
     engine.addImportPath("_deps/mydesigns-build/");
     engine.addImportPath("_deps/account-build/");
     engine.addImportPath("_deps/nodeconection-build/");
     qDebug()<<engine.importPathList();
 
-    const QUrl url(u"qrc:/esthertech.com/imports/NFT_Minter/qml/window.qml"_qs);
+    const QUrl url(u"qrc:/esterVtech.com/imports/NFT_Minter/qml/window.qml"_qs);
 	QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
 			&app, [url](QObject *obj, const QUrl &objUrl) {
 			if (!obj && url == objUrl)
