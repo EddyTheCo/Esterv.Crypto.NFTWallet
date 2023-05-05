172bf9d9b4fed409c61255750ea1d26ed5cf81f3
EddyGlezz <eddyglezz95@gmail.com>
2023-05-05 10:39:33 +0200

    added the custom style registration and set the color of qr

--- main.cpp
+++ main.cpp
@@ -9,14 +9,15 @@ int main(int argc, char *argv[])
 	QGuiApplication app(argc, argv);
 
 	QQmlApplicationEngine engine;
-	engine.addImageProvider(QLatin1String("qrcodeblack"), new QRImageProvider("black",1));
+    engine.addImageProvider(QLatin1String("qrcodeblack"), new QRImageProvider("#10141c",1));
     engine.addImportPath("qrc:/esterVtech.com/imports");
     engine.addImportPath("_deps/mydesigns-build/");
     engine.addImportPath("_deps/account-build/");
     engine.addImportPath("_deps/nodeconection-build/");
     qDebug()<<engine.importPathList();
 
-    const QUrl url(u"qrc:/esterVtech.com/imports/NFT_Minter/qml/window.qml"_qs);
+    qmlRegisterSingletonType(QUrl(u"qrc:/esterVtech.com/imports/MyDesigns/qml/CustomStyle.qml"_qs), "CustomStyle", 1, 0, "CustomStyle");
+    const QUrl url(u"qrc:/esterVtech.com/imports/NftMinter/qml/window.qml"_qs);
 	QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
 			&app, [url](QObject *obj, const QUrl &objUrl) {
 			if (!obj && url == objUrl)
