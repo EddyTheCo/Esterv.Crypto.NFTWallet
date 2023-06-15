9375aa352a9719947c66fcff952fc8eb31e9b828
EddyGlezz <eddyglezz95@gmail.com>
2023-06-15 12:16:11 +0200

    added the buyme a coffe to page

--- wasm/js/scripts.js
+++ wasm/js/scripts.js
@@ -1,10 +1,26 @@
 function init() {
-	initQTwasm('.', 'nftminter', '#qtrootDiv', 'img/qtlogo.svg');
+	qtMint=initQTwasm('.', 'nftminter', '#qtrootDiv', 'img/qtlogo.svg');
+	qtBuy=initQTwasm('https://eddytheco.github.io/BuyMeACoffe/', 'buymeacoffe', '#buyme', '');
+	checkModuleLoad=setInterval(function() {
+                  if (qtBuy.module())
+                  {
+                          qtBuy.module().Monitor.get_monitor().set_properties("https://api.shimmer.network","smr1qq9j9n0wmqu7fhermm6x7lpw35zd8dn24vcty62u06wt7g0fa7fukh98hvc");
+                          clearInterval(checkModuleLoad);
+                  }
+  
+                  if( typeof counter == 'undefined' ) {
+                          counter = 0;
+                  }
+                  counter++;
+                  if(counter>60)clearInterval(checkModuleLoad);
+          }, 1000);
 
 }
 
 
 function resizeSplitX(event) {
-	const canvas = document.querySelector('#screen');
-	qtLoader.resizeCanvasElement(canvas);
+	const canvasMint = document.querySelector('#screennftminter');
+	qtMint.resizeCanvasElement(canvasMint);
+	const canvasBuy = document.querySelector('#screenbuymeacoffe');
+	qtBuy.resizeCanvasElement(canvasBuy);
 }
