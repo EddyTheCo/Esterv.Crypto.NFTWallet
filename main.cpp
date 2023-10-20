955ec5220bbf13b1a41aa110ce1286f6359f301f
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2023-10-20 16:26:04 +0200

    removed force linking and install plugin (#47) (#48)

--- main.cpp
+++ main.cpp
@@ -2,15 +2,8 @@
 #include <QQmlApplicationEngine>
 #include "Qrimageprovider.hpp"
 #include "Qrimagedecoder.hpp"
-#include "mydesigns.hpp"
-#include "midlePay.hpp"
 int main(int argc, char *argv[])
 {
-    auto foo=fooDesign::fooPrint(); //https://forum.qt.io/post/762513
-    foo=fooQtQrGen::fooPrint();
-    foo=fooQtQrDec::fooPrint();
-    foo=fooMidlePay::fooPrint();
-
 
     QGuiApplication app(argc, argv);
 
@@ -22,12 +15,6 @@ int main(int argc, char *argv[])
 
     qmlRegisterSingletonType(QUrl(u"qrc:/esterVtech.com/imports/MyDesigns/qml/CustomStyle.qml"_qs), "CustomStyle", 1, 0, "CustomStyle");
     const QUrl url(u"qrc:/esterVtech.com/imports/NftMinter/qml/window.qml"_qs);
-	QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-			&app, [url](QObject *obj, const QUrl &objUrl) {
-			if (!obj && url == objUrl)
-			QCoreApplication::exit(-1);
-			}, Qt::QueuedConnection);
-
 	engine.load(url);
 
 	return app.exec();
