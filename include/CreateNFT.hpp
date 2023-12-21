087544f061ffc667c5f13220d1fb61ac58b2663f
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2023-12-21 12:05:03 +0100

    init using the wallet

--- include/CreateNFT.hpp
+++ include/CreateNFT.hpp
@@ -5,173 +5,103 @@
 #include<QString>
 #include <QtQml/qqmlregistration.h>
 
-#include<account.hpp>
-#include"nodeConnection.hpp"
-#include"outmonitor.hpp"
 #include<QHash>
 
+#include "block/qoutputs.hpp"
 
 
+using namespace  qiota;
+using namespace  qblocks;
 
 class NftBox :public QObject
 {
-	Q_OBJECT
+    Q_OBJECT
 
-		Q_PROPERTY(QString  issuer READ issuer CONSTANT)
-		Q_PROPERTY(QString  address READ address CONSTANT)
-		Q_PROPERTY(QString  metdata READ metdata WRITE setMetdata NOTIFY metdataChanged)
-		QML_ELEMENT
+    Q_PROPERTY(QString  issuer READ issuerBech32 WRITE setIssuer NOTIFY issuerChanged)
+    Q_PROPERTY(QString  address READ addressBech32 NOTIFY addressChanged)
+    Q_PROPERTY(QString  metdata READ metdata WRITE setMetdata NOTIFY metdataChanged)
+    QML_ELEMENT
 
-	public:
-		NftBox(QObject *parent = nullptr):QObject(parent){}
-		NftBox(std::shared_ptr<Output> out,QObject *parent = nullptr);
+public:
+    NftBox(QObject *parent = nullptr):QObject(parent){}
+    NftBox(std::shared_ptr<const Output> out, QObject *parent = nullptr);
 
+    static bool set_addr(QString var_str,c_array& var);
 
-		static QString get_addr(const c_array& var);
+    QString metdata()const{return (m_data.isEmpty())?QString():QString(m_data);}
+    QString issuerBech32()const{return m_issuerBech32;}
+    QString addressBech32()const{return m_addressBech32;}
+    c_array addrArray()const{return m_address;}
+    c_array issuerArray()const{return m_issuer;}
+    c_array dataArray()const{return m_data;}
 
-		static bool set_addr(QString var_str,c_array& var);
+    void setMetdata(QString data);
+    void setIssuer(QString addr)
+    {
+        if(NftBox::set_addr(addr,m_issuer))emit issuerChanged();
+    }
 
-		QString metdata()const{return (data_.isEmpty())?QString():QString(data_);}
-		QString issuer()const{return get_addr(issuer_);}
-		QString address()const{return get_addr(address_);}
-		c_array addr_array()const{return address_;}
-		c_array data_array()const{return data_;}
-
-		void setMetdata(QString data);
-
-
-	signals:
-		void issuerChanged();
-		void addressChanged();
-		void metdataChanged();
-	private:
-		c_array issuer_,address_,data_;
+signals:
+    void issuerChanged();
+    void addressChanged();
+    void metdataChanged();
+private:
+    c_array m_issuer,m_address,m_data;
+    QString m_addressBech32,m_issuerBech32;
 
 };
 
 class BoxModel : public QAbstractListModel
 {
-	Q_OBJECT
-		Q_PROPERTY(int count READ count NOTIFY countChanged)
-		Q_PROPERTY(int newBoxes READ newBoxes NOTIFY newBoxesChanged)
-		Q_PROPERTY(QString cissuer READ cissuer WRITE setCissuer NOTIFY cIssuerChanged)
-		QML_ELEMENT
+    Q_OBJECT
+    Q_PROPERTY(int count READ count NOTIFY countChanged)
+    Q_PROPERTY(int newBoxes READ newBoxes NOTIFY newBoxesChanged)
+    QML_ELEMENT
+    QML_SINGLETON
 
-	public:
-		enum ModelRoles {
-			issuerRole = Qt::UserRole + 1,
-			metdataRole,addressRole};
+public:
+    enum ModelRoles {
+        issuerRole = Qt::UserRole + 1,
+        metdataRole,addressRole};
 
-		int count() const;
-		int newBoxes()const{return newBoxes_;}
-		explicit BoxModel(QObject *parent = nullptr);
-		pvector<const Output> newNFTs(Node_info*,std::shared_ptr<const Unlock_Condition> unlock);
-		void clearBoxes(bool emptyones=true);
-		void setCissuer(QString addr)
-		{
-			if(NftBox::set_addr(addr,cIssuer_))emit cIssuerChanged();
-		}
-		QString cissuer()const
-		{
-			return NftBox::get_addr(cIssuer_);
-		}
-		Q_INVOKABLE void newBox(void)
-		{
-			addBox(new NftBox(this));
-			newBoxes_++;
-			emit newBoxesChanged();
-		};
-		Q_INVOKABLE void unsetCissuer(void)
-		{
-			cIssuer_=c_array();
-			emit cIssuerChanged();
-		};
-		void addBox(NftBox* nbox);
-		Q_INVOKABLE void rmBox(int i);
-		void rmBoxId(c_array addr);
+    int count() const;
+    int newBoxes()const{return newBoxes_;}
+    BoxModel(QObject *parent = nullptr);
 
-		Q_INVOKABLE bool setProperty(int i, QString role, const QVariant value);
+    Q_INVOKABLE void clearBoxes(bool emptyones=true);
+    Q_INVOKABLE void mint(){};
+
+    Q_INVOKABLE void newBox(void)
+    {
+        addBox(new NftBox(this));
+        newBoxes_++;
+        emit newBoxesChanged();
+    };
+    void addBox(NftBox* nbox);
+    Q_INVOKABLE void rmBox(int i);
+    void rmBoxId(c_array addr);
+
+    Q_INVOKABLE bool setProperty(int i, QString role, const QVariant value);
 
 
-		int rowCount(const QModelIndex &p) const;
-		QVariant data(const QModelIndex &index, int role) const;
-		bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole);
-		QModelIndex index(int row, int column = 0, const QModelIndex &parent = QModelIndex()) const;
-		QHash<int, QByteArray> roleNames() const;
+    int rowCount(const QModelIndex &p) const;
+    QVariant data(const QModelIndex &index, int role) const;
+    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole);
+    QModelIndex index(int row, int column = 0, const QModelIndex &parent = QModelIndex()) const;
+    QHash<int, QByteArray> roleNames() const;
 
-	signals:
-		void countChanged(int count);
-		void cIssuerChanged();
-		void newBoxesChanged();
+signals:
+    void countChanged(int count);
+    void cIssuerChanged();
+    void newBoxesChanged();
 
-	private:
-		int m_count,newBoxes_;
-		QList<NftBox*> boxes;
-		c_array cIssuer_;
+private:
+    int m_count,newBoxes_;
+    QList<NftBox*> boxes;
 };
 
 
-class NFTCreator : public QObject
-{
 
-	Q_OBJECT
-
-		Q_PROPERTY(QJsonObject  funds READ funds  NOTIFY fundsChanged)
-		Q_PROPERTY(BoxModel*  model READ model  CONSTANT)
-		Q_PROPERTY(Stte  state READ state  NOTIFY stateChanged)
-		Q_PROPERTY(QString recAddress READ recAddress WRITE setRecAddress NOTIFY recAddressChanged)
-		QML_ELEMENT
-		QML_SINGLETON
-
-	public:
-		NFTCreator(QObject *parent = nullptr);
-		enum Stte {
-			Null,
-			Ready,
-		};
-		Q_ENUM(Stte)
-			Q_INVOKABLE void mint(bool issend=false);
-		Q_INVOKABLE void send(void){mint(true);}
-		Q_INVOKABLE void restart(void);
-		Stte state()const{return state_;}
-		void setState(Stte state_m){if(state_m!=state_){state_=state_m;emit stateChanged();}};
-
-		void setRecAddress(QString addr)
-		{
-			if(NftBox::set_addr(addr,recAddress_))emit recAddressChanged();
-		}
-		QString recAddress()const
-		{
-			return NftBox::get_addr(recAddress_);
-		}
-
-		QJsonObject funds(void)const{return funds_json;}
-		void setFunds(quint64 funds_m);
-		BoxModel* model()const{return model_;}
-
-
-	signals:
-		void fundsChanged();
-		void created(void);
-		void newBlock(QString);
-		void notEnought(QJsonObject);
-		void stateChanged();
-		void recAddressChanged();
-
-	private:
-
-		void checkOutputs(std::vector<qiota::Node_output>  outs);
-		void createdBlock(qblocks::c_array);
-		OutMonitor* monitor;
-		quint64 funds_;
-		QJsonObject funds_json;
-		QObject* receiver;
-		QHash<QString,quint64> total_funds;
-		BoxModel* model_;
-		Stte state_;
-		c_array recAddress_;
-
-};
 
 
 
--- qml/window.qml
+++ qml/window.qml
@@ -2,11 +2,8 @@ import QtQuick.Controls
 import QtQuick
 import QtQuick.Layouts
 
-import account
-import NftMinter
-import MyDesigns
-import nodeConection
-import midlePay
+import Esterv.Styles.Simple
+import Esterv.Iota.NodeConnection
 
 ApplicationWindow {
     visible: true
@@ -14,138 +11,69 @@ ApplicationWindow {
 
     FontLoader {
         id: nftFont
-        source: "qrc:/esterVtech.com/imports/NftMinter/fonts/Anton/Anton-Regular.ttf"
+        source: "qrc:/esterVtech.com/imports/Esterv/Iota/NFTMinter/fonts/Anton/Anton-Regular.ttf"
     }
     FontLoader {
         id: amountFont
-        source: "qrc:/esterVtech.com/imports/NftMinter/fonts/Permanent_Marker/PermanentMarker-Regular.ttf"
+        source: "qrc:/esterVtech.com/imports/Esterv/Iota/NFTMinter/fonts/Permanent_Marker/PermanentMarker-Regular.ttf"
     }
     background: Rectangle
     {
-        color:CustomStyle.backColor1
+        color:Style.backColor1
     }
     Component.onCompleted:
     {
-        if(LocalConf.nodeaddr) Node_Conection.nodeaddr=LocalConf.nodeaddr;
-        if(LocalConf.jwt) Node_Conection.jwt=LocalConf.jwt;
-        CustomStyle.h1=Qt.font({
-                                   family: nftFont.font.family,
-                                   weight: nftFont.font.weight,
-                                   pixelSize: 28
-                               });
-        CustomStyle.h2=Qt.font({
-                                   family: amountFont.font.family,
-                                   weight: amountFont.font.weight,
-                                   pixelSize: 28
-                               });
-    }
-
-
-    Connections {
-        target: Account
-        function onSeedChanged() {
-            NFTCreator.restart();
-        }
-    }
-
-    Connections {
-        target: NFTCreator
-        function onNotEnought(amount) {
-            noti.show({"message":"Not enough funds\n "+ "lack of "+ amount.largeValue.value + " "+ amount.largeValue.unit });
-        }
-    }
-
-    Connections {
-        target: Node_Conection
-        function onStateChanged() {
-            if(Node_Conection.state==Node_Conection.Connected)
-            {
-                noti.show({"message":"Conected to "+ Node_Conection.nodeaddr });
-            }
-            NFTCreator.restart();
-        }
-    }
-    MidlePayPop
-    {
-        initTag:"NFTCreator"
-        width:300
-        height:350
-        anchors.centerIn: Overlay.overlay
+        Style.h1=Qt.font({
+                             family: nftFont.font.family,
+                             weight: nftFont.font.weight,
+                             pixelSize: 28
+                         });
+        Style.h2=Qt.font({
+                             family: amountFont.font.family,
+                             weight: amountFont.font.weight,
+                             pixelSize: 28
+                         });
     }
 
     GridLayout
     {
         id:grid
+        property bool iscolumn:width < 750
         width:window.width-drawer.width*((window.width>500)?drawer.position:0.0)
         height:drawer.height
         x:drawer.width*drawer.position
-        columns: width > 750 ? 2 : 1
-        rows : width > 750 ? 1 : 2
-        rowSpacing: 10
-
-
-        ListView {
-            id:boxes
-
+        flow: grid.iscolumn ? GridLayout.TopToBottom : GridLayout.LeftToRight
+        Rectangle
+        {
+            color:"red"
             Layout.alignment: Qt.AlignTop
             Layout.margins:  15
             Layout.fillHeight: true
             Layout.fillWidth: true
-            Layout.minimumWidth: (grid.columns===1)?150:450
-            spacing : 10
-            clip:true
-
-            model: NFTCreator.model
-
-            BusyIndicator {
-                anchors.top: boxes.top
-                anchors.horizontalCenter: boxes.horizontalCenter
-                anchors.margins: 20
-                running: NFTCreator.state===NFTCreator.Null
-            }
-
-
-            delegate: BoxNFT {
-                width: ListView.view.width
-
-            }
-
+            Layout.minimumWidth: (grid.iscolumn)?150:450
         }
+
         BoxMenu
         {
             id:boxmenu
-            Layout.alignment: (grid.columns===1)?Qt.AlignBottom|Qt.AlignHCenter:Qt.AlignVCenter|Qt.AlignRight
+            onShowSettings: (drawer.position>0)?drawer.close():drawer.open();
+            Layout.alignment: (grid.iscolumn)?Qt.AlignBottom|Qt.AlignHCenter:Qt.AlignVCenter|Qt.AlignRight
             Layout.fillHeight: true
             Layout.fillWidth: true
-            Layout.topMargin: (grid.columns===1)?15:0
-            Layout.leftMargin: (grid.columns===1)?0:15
-            Layout.maximumWidth: 500-150*(grid.columns-1)
-            Layout.maximumHeight: 200+250*(grid.columns-1)
-            Layout.minimumWidth: (grid.columns===1)?150:250
-            settings.animate:drawer.visible
-            onShowSettings:(drawer.position>0)?drawer.close():drawer.open();
-
+            Layout.topMargin: (grid.iscolumn)?15:0
+            Layout.leftMargin: (grid.iscolumn)?0:15
+            Layout.maximumWidth: 500-((grid.iscolumn)?150:0)
+            Layout.maximumHeight: 200+((grid.iscolumn)?0:250)
+            Layout.minimumWidth: (grid.iscolumn)?150:250
         }
-
-
     }
+
+
+
     ConfDrawer
     {
         id:drawer
-        width: 300
+        width: Math.max(parent.width*0.2,350)
         height: window.height
     }
-
-
-
-    Notification
-    {
-        id:noti
-        width:360
-        height:100
-        x:(window.width-width)*0.5
-        y: window.height*(1-0.05)-height
-    }
-
-
 }
