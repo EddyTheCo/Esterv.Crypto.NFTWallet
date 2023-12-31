48bf3e1707b42cc24229220ee010d067e7ebd92d
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2023-12-31 09:21:08 +0100

    do not use QRdec if target is not present

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include "Qrimageprovider.hpp"
+#if defined(USE_QRDEC)
 #include "Qrimagedecoder.hpp"
-
+#endif
 #if defined(FORCE_STYLE)
 #include <QQuickStyle>
 #endif
@@ -16,7 +17,9 @@ int main(int argc, char *argv[])
 
 	QQmlApplicationEngine engine;
     engine.addImageProvider(QLatin1String("qrcode"), new QRImageProvider(1));
+#if defined(USE_QRDEC)
     engine.addImageProvider(QLatin1String("wasm"), new WasmImageProvider());
+#endif
     engine.addImportPath("qrc:/esterVtech.com/imports");
 
     const QUrl url(u"qrc:/esterVtech.com/imports/Esterv/Iota/NFTMinter/qml/window.qml"_qs);
