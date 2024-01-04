5d528bf6d7081212e082e34dacbe6187ef021de1
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2024-01-04 15:23:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include "Qrimageprovider.hpp"
-#if defined(USE_QRDEC)
 #include "Qrimagedecoder.hpp"
-#endif
 #if defined(FORCE_STYLE)
 #include <QQuickStyle>
 #endif
@@ -17,9 +15,7 @@ int main(int argc, char *argv[])
 
 	QQmlApplicationEngine engine;
     engine.addImageProvider(QLatin1String("qrcode"), new QRImageProvider(1));
-#if defined(USE_QRDEC)
     engine.addImageProvider(QLatin1String("wasm"), new WasmImageProvider());
-#endif
     engine.addImportPath("qrc:/esterVtech.com/imports");
 
     const QUrl url(u"qrc:/esterVtech.com/imports/Esterv/Iota/NFTMinter/qml/window.qml"_qs);
